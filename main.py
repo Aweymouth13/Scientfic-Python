d343ac85ddb512e6868b6d8f2881931723cf0255
aaron <aaron.weymouth@yahoo.com>
2023-10-12 21:04:57 -0500

    hw8

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import scipy
-
-!wget https://raw.githubusercontent.com/thaynecurrie/phys7943_fall2023/main/ScientificPythonNotes/_problemsets_github/week6/diskmasses.txt
-print('hello world')
+import matplotlib.pyplot as plt
+from matplotlib import style
+from matplotlib import rcParams
+#sets style to 538
+plt.style.use('fivethirtyeight')
+#sets rcParams to 12 x 8
+rcParams['figure.figsize'] = 12, 8
+print('hi')
